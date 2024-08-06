18995879d0c042234766500adb735c180abd9a47
eulabconsult <eulabconsult@gmail.com>
2024-08-06 18:20:38 +0200

    Fix API_KEY

--- src/app/translate.service.ts
+++ src/app/translate.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GoogleObj } from './model/googleObj';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TranslateService {
   url = 'https://translation.googleapis.com/language/translate/v2?key=';
-  key = 'AIzaSyBLEzB5bSzBEx7XNEQS-yaqtOGlxAJ1FIo';
+  key = environment.GOOGLE_API_KEY;
   constructor(private http: HttpClient) {}
   translate(obj: GoogleObj): any {
     return this.http.post(this.url + this.key, obj);
